0d7a3ebce95e4547c6af4d0e07f79b1a2e71ad57
Akshay Naval <akshay.naval@gmail.com>
2013-11-10 18:00:03 -0500

    Akshay: Hooking in filereader in algorhythms class

--- AlgoRhythms.java
+++ AlgoRhythms.java
@@ -1,6 +1,6 @@
 public class AlgoRhythms {
     public static void main(String[] args) {
-        String input = "asdasdsad";
+        String input = FileReader.readFile(args[0]);
         Huffman huffman = new Huffman();
         String huffmanCompression = huffman.compress(input);
         System.out.println("The compressed String after applying huffman is: " + huffmanCompression);
