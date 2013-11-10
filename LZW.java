ceb0db903c0ab30cccaa9f781d994f5f07208dc0
Go Wind <govindappumenon@gmail.com>
2013-11-10 15:08:55 -0500

    lzw | Govind | Variable length codes for lzw

--- LZW.java
+++ LZW.java
@@ -1,19 +1,35 @@
 import java.io.*;
+import java.lang.Math.*;
 import java.util.*;
 public class LZW {
 
+	public static String toBinary(int n,int wordSize) {
+		int i;
+		String res = "";
+		for(i=0;i<wordSize;i++)
+		{
+			res = res + String.valueOf(n % 2);
+			n = n/2;
+
+		}
+		return new StringBuilder(res).reverse().toString();
+	}
+
 	public static void main(String[] args) throws IOException {
 		List dictionary = new ArrayList();
-		int i,j;
 		String input = "TOBEORNOTTOBEORTOBEORNOT";
+		String output = "";
 		String buffer;
-
+		int wordSize;
+		int i,j;
+		
 		for(i=0;i<26;i++)
 			dictionary.add(String.valueOf((char)(i+65)));
 		
 		i=0;
 		//System.out.println(input.length());
 		while(i<input.length()) {
+			wordSize =(int) Math.ceil( Math.log(dictionary.size()) / Math.log(2)) ;
 			j = i;
 			buffer = "";
 
@@ -24,18 +40,20 @@ public class LZW {
 				buffer = buffer + input.charAt(j);
 				j =j +1;
 			}
-			
+
 			if(j>=input.length())
 				break;
 			
-			System.out.print(dictionary.indexOf(buffer)+1);
-			System.out.print(' ');
-			System.out.println(buffer + input.charAt(j));
-
+			//System.out.print(dictionary.indexOf(buffer)+1);
+			//System.out.print(' ');
+			//System.out.println(buffer + input.charAt(j));
+			output += toBinary(dictionary.indexOf(buffer),wordSize);
+			//System.out.println(toBinary(dictionary.indexOf(buffer) +1,wordSize));
 			dictionary.add(buffer + input.charAt(j));
 
 			i = j;
 
 		}
+		System.out.println(output);
 	}
 }
